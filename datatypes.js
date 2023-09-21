fc1a127d884297c59bf9eb66225d94bde14edf5b
Aymen13h <aymen.kharrat13@gmail.com>
2023-09-21 14:55:53 +0100

    primitive and Objects

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+var number = 10;
+var text = " I am a string";
+var cars = ["merc", "BMW", "Toyota"];
+var x = {firstName: "John", lastName: "Doe"};
+var condition = true;
+
+console.log(number, typeof number);
+console.log(text, typeof text);
+console.log(cars, typeof cars);
+console.log(x, typeof x)
+console.log(condition, typeof condition);
+
+var x = 10;
+console.log(x, typeof x);
+
+var number1 = 10.35;
+var expo = 123e-5
+
+console.log(cars[1]);
+
+var person = {firstName: "John", lastName : "Doe", age: 25, dob: "12/12/1981"}
+console.log(person.firstName, person.lastName, person.age);
+
+var abhay = null;
+console.log( null, typeof null );
+console.log(undefined, typeof undefined);
